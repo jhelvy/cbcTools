8eb10cc3e83c189d56ecc20fd3e1bf6ab9cc0175
John Helveston <john.helveston@gmail.com>
2022-01-12 15:34:58 -0500

    finished making randomized design

--- R/cbc_design.R
+++ R/cbc_design.R
@@ -1,35 +1,27 @@
-#' Make a choice-based conjoint survey design
+#' Make a random or D-efficient choice-based conjoint survey design
 #'
-#' @param levels A named list of vectors defining each attribute (the names)
-#' and each level for each attribute (the vectors). For example, a design
-#' with two attributes, `"price"` and `"type"`, that each had three levels
-#' should be defined as `levels = list(price = c(1, 2, 3), type = c("1", "2", "3"))`.
-#' Numeric values will be treated as continuous levels and character values
-#' will be treated as discrete levels.
-#' @param nResp Maximum number of survey respondents.
-#' @param nAltsPerQ Number of alternatives per question.
-#' @param nQPerResp Number of questions per respondent.
-#' @param outsideGood Include an outside good in the choice sets? Defaults to
+#' This function creates a data frame containing a choice-based conjoint survey
+#' design where each row is an alternative. Designs can be either fully
+#' randomized or D-efficient, in which case an implementation of the
+#' Modified Federov algorithm is used via the `idefix` package.
+#'
+#' @param profiles A numeric matrix in which each row is a possible profile.
+#' This can be generated using the `cbc_profiles()` function.
+#' @param n_resp Number of survey respondents.
+#' @param n_alts Number of alternatives per choice question.
+#' @param n_q Number of questions per respondent.
+#' @param no_choice Include a "none" option in the choice sets? Defaults to
 #' `FALSE`. If `TRUE`, the total number of alternatives per question will be
-#' one more than the provided `nAltsPerQ` argument.
+#' one more than the provided `n_alts` argument.
 #' @param random Set to `TRUE` to return a fully randomized design, otherwise
 #' returns a D-efficient design. Defaults to `TRUE`.
 #' @param label The name of the variable to use in a "labeled" design
 #' such that each set of alternatives contains one of each of the levels in
-#' the `label` attribute. If included, the `nAltsPerQ` argument will be
+#' the `label` attribute. If included, the `n_alts` argument will be
 #' ignored as its value is determined by the unique number of levels in the
 #' `label` variable. Defaults to `NULL`.
-#' @param maxTrials The maximum number of trials to be used in a fractional
-#' factorial design. Defaults to `NA`, in which case it is set to the number of
-#' alternatives in the full factorial design. If `search = TRUE`, then all
-#' feasible designs will be calculated up to `maxTrials` or until a perfect
-#' D-efficiency (1) is found.
-#' @param search If `TRUE`, all feasible designs are calculated up to
-#' `maxTrials` or until a perfect D-efficiency (1) is found, after which a
-#' summary of the search results is printed and the top-ranked D-efficient
-#' design is returned. Defaults to `FALSE`.
-#' @return Returns a data frame containing a choice-based conjoint survey
-#' design.
+#' @return A data frame containing a choice-based conjoint survey design where
+#' each row is an alternative.
 #' @export
 #' @examples
 #' # Define the attributes and levels
@@ -39,77 +31,72 @@
 #'   freshness = c('Excellent', 'Average', 'Poor')
 #' )
 #'
-#' # Make a full-factorial design of experiment
-#' doe <- makeDoe(levels)
+#' # Generate all profiles with price as a continuous variable
+#' profiles <- cbc_profiles(levels, coding = c("C", "D", "D"))
 #'
-#' # Re-code levels
-#' doe <- recodeDoe(doe, levels)
-#'
-#' # Make the conjoint survey by randomly sampling from the doe
+#' # Make a fully random conjoint survey
 #' survey <- cbc_design(
-#'   doe       = doe,  # Design of experiment
-#'   nResp     = 2000, # Total number of respondents (upper bound)
-#'   nAltsPerQ = 3,    # Number of alternatives per question
-#'   nQPerResp = 6     # Number of questions per respondent
+#'   profiles = profiles,
+#'   n_resp   = 300, # Number of respondents
+#'   n_alts   = 3,   # Number of alternatives per question
+#'   n_q      = 6,   # Number of questions per respondent
+#' )
+#'
+#' # Make a D-efficient conjoint survey
+#' survey <- cbc_design(
+#'   profiles = profiles,
+#'   n_resp   = 300, # Number of respondents
+#'   n_alts   = 3,   # Number of alternatives per question
+#'   n_q      = 6    # Number of questions per respondent
+#'   random   = FALSE
 #' )
 cbc_design <- function(
-    levels,
-    nResp,
-    nAltsPerQ,
-    nQPerResp,
-    outsideGood = FALSE,
+    profiles,
+    n_resp,
+    n_alts,
+    n_q,
+    no_choice = FALSE,
     random = TRUE,
-    label = NULL,
-    maxTrials = NA,
-    search = FALSE
+    label = NULL
 ) {
-    doe <- as.data.frame(doe) # tibbles break things
-    doe$rowID <- seq(nrow(doe)) # Have to set these now to remove dupes later
-    if (is.null(label)) {
-        return(randomizeSurvey(doe, nResp, nAltsPerQ, nQPerResp, outsideGood))
+    profiles <- as.data.frame(profiles) # tibbles break things
+    profiles$id <- seq(nrow(profiles)) # Set these to remove dupes later
+    if (random) {
+        design <- make_design_rand(
+            profiles, n_resp, n_alts, n_q, no_choice, label)
+    } else {
+        design <- make_design_eff(
+            profiles, n_resp, n_alts, n_q, no_choice, label)
     }
-    return(randomizeSurveyLabeled(
-        doe, nResp, nAltsPerQ, nQPerResp, outsideGood, label))
+    return(design)
 }
 
-repDf <- function(df, n) {
-    return(df[rep(seq_len(nrow(df)), n), ])
-}
-
-randomizeSurvey <- function(doe, nResp, nAltsPerQ, nQPerResp, outsideGood) {
-    survey <- initializeSurvey(doe, nResp, nAltsPerQ, nQPerResp)
-    # Randomize rows
-    n <- nResp*nAltsPerQ*nQPerResp
-    survey <- randomizeRows(survey, n)
-    # Add meta data and remove cases with double alternatives
-    survey <- addMetaData(survey, nResp, nAltsPerQ, nQPerResp)
-    survey <- removeDuplicateAlts(survey, nResp, nAltsPerQ, nQPerResp)
-    # Re-order column names
-    survey <- reorderSurveyCols(survey)
-    if (outsideGood) {
-        survey <- addOutsideGood(survey, nAltsPerQ)
+make_design_rand <- function(profiles, n_resp, n_alts, n_q, no_choice, label) {
+    design <- sample_rows(profiles, n = n_resp*n_alts*n_q)
+    design <- remove_dups(design, n_resp, n_alts, n_q)
+    design <- reorder_cols(design)
+    if (no_choice) {
+        design <- add_no_choice(design, n_alts)
     }
-    return(survey)
+    return(design)
 }
 
-randomizeSurveyLabeled <- function(
-    doe, nResp, nAltsPerQ, nQPerResp, outsideGood, label
-) {
+make_design_eff <- function(profiles, n_resp, n_alts, n_q, no_choice, label) {
     nLabelLevels <- length(unique(doe[,label]))
-    if (nLabelLevels != nAltsPerQ) {
+    if (nLabelLevels != n_alts) {
         warning(paste0(
-            "The nAltsPerQ argument is being set to ", nLabelLevels,
+            "The n_alts argument is being set to ", nLabelLevels,
             " to match the number of unique levels in the ", label, " variable"
         ))
-        # Over-ride user-provided nAltsPerQ as it is determined by the label
-        nAltsPerQ <- nLabelLevels
+        # Over-ride user-provided n_alts as it is determined by the label
+        n_alts <- nLabelLevels
     }
-    survey <- initializeSurvey(doe, nResp, nAltsPerQ, nQPerResp, label)
+    survey <- initializeSurvey(doe, n_resp, n_alts, n_q, label)
     # Randomize rows by label
     labels <- split(survey, survey[label])
     for (i in seq_len(length(labels))) {
-        n <- nResp*nQPerResp
-        survey_label <- randomizeRows(labels[[i]], n)
+        n <- n_resp*n_q
+        survey_label <- randomize_rows(labels[[i]], n)
         survey_label$labelID <- seq(nrow(survey_label))
         labels[[i]] <- survey_label
     }
@@ -117,114 +104,72 @@ randomizeSurveyLabeled <- function(
     survey <- survey[order(survey$labelID),]
     survey$labelID <- NULL
     # Add meta data and remove cases with double alternatives
-    survey <- addMetaData(survey, nResp, nAltsPerQ, nQPerResp)
+    survey <- add_metadata(survey, n_resp, n_alts, n_q)
     # Re-order column names
-    survey <- reorderSurveyCols(survey)
-    if (outsideGood) {
-        survey <- addOutsideGood(survey, nAltsPerQ)
+    survey <- reorder_cols(survey)
+    if (no_choice) {
+        survey <- addno_choice(survey, n_alts)
     }
     return(survey)
 }
 
-initializeSurvey <- function(doe, nResp, nAltsPerQ, nQPerResp, label = NULL) {
-    # Replicate doe if the needed number of observations (n) is larger than
-    # the number of observations in the doe (n_doe)
-    n <- nResp*nAltsPerQ*nQPerResp
-    n_doe <- nrow(doe)
-    nReps <- ceiling(n / n_doe)
-    if (!is.null(label)) {
-        # If there is a label, then need to replicate based on the
-        # lowest number of any one label
-        n_labels <- min(table(doe[label]))
-        n <- nResp*nQPerResp
-        nReps <- ceiling(n / n_labels)
+sample_rows <- function(profiles, n) {
+    sample_ids <- sample(x = seq_len(nrow(profiles)), size = n, replace = TRUE)
+    return(profiles[sample_ids,])
+}
+
+remove_dups <- function(design, n_resp, n_alts, n_q) {
+    design <- add_metadata(design, n_resp, n_alts, n_q)
+    dup_rows <- get_dups(design, n_alts)
+    while (length(dup_rows) > 0) {
+        # cat('Number repeated: ', length(dup_rows), '\n')
+        new_rows <- sample(
+            x = seq(nrow(design)), size = length(dup_rows), replace = F)
+        design[dup_rows,] <- design[new_rows,]
+        design <- add_metadata(design, n_resp, n_alts, n_q)
+        dup_rows <- get_dups(design, n_alts)
     }
-    survey <- doe
-    if (nReps > 1) {
-        survey <- repDf(doe, nReps)
-    }
-    row.names(survey) <- NULL
-    return(survey)
+    return(design)
 }
 
-randomizeRows <- function(survey, n) {
-    sample_ids <- sample(x = seq_len(nrow(survey)), size = n, replace = FALSE)
-    return(survey[sample_ids,])
+add_metadata <- function(design, n_resp, n_alts, n_q) {
+    n_rows_per_resp   <- n_alts*n_q
+    design$respID     <- rep(seq(n_resp), each = n_rows_per_resp)
+    design$qID        <- rep(rep(seq(n_q), each = n_alts), n_resp)
+    design$altID      <- rep(seq(n_alts), n_resp*n_q)
+    design$obsID      <- rep(seq(n_resp * n_q), each = n_alts)
+    row.names(design) <- NULL
+    return(design)
 }
 
-addMetaData <- function(survey, nResp, nAltsPerQ, nQPerResp) {
-    nRowsPerResp      <- nAltsPerQ*nQPerResp
-    survey$respID     <- rep(seq(nResp), each = nRowsPerResp)
-    survey$qID        <- rep(rep(seq(nQPerResp), each = nAltsPerQ), nResp)
-    survey$altID      <- rep(seq(nAltsPerQ), nResp*nQPerResp)
-    survey$obsID      <- rep(seq(nResp * nQPerResp), each = nAltsPerQ)
-    row.names(survey) <- NULL
-    return(survey)
-}
-
-removeDuplicateAlts <- function(survey, nResp, nAltsPerQ, nQPerResp) {
-    duplicateRows <- getDuplicateRows(survey, nAltsPerQ)
-    while (length(duplicateRows) > 0) {
-        # cat('Number repeated: ', length(duplicateRows), '\n')
-        newRows <- sample(
-            x = seq(nrow(survey)), size = length(duplicateRows), replace = F)
-        survey[duplicateRows,] <- survey[newRows,]
-        survey <- addMetaData(survey, nResp, nAltsPerQ, nQPerResp)
-        duplicateRows <- getDuplicateRows(survey, nAltsPerQ)
-    }
-    return(survey)
-}
-
-getDuplicateRows <- function(survey, nAltsPerQ) {
-    counts <- tapply(survey$rowID, survey$obsID,
+get_dups <- function(design, n_alts) {
+    counts <- tapply(design$id, design$obsID,
                      FUN = function(x) length(unique(x)))
-    duplicateIDs <- which(counts != nAltsPerQ)
-    duplicateRows <- which(survey$obsID %in% duplicateIDs)
-    return(duplicateRows)
+    dup_ids <- which(counts != n_alts)
+    dup_rows <- which(design$obsID %in% dup_ids)
+    return(dup_rows)
 }
 
-reorderSurveyCols <- function(survey) {
+reorder_cols <- function(design) {
     metaNames <- c("respID", "qID", "altID", "obsID")
-    varNames <- setdiff(names(survey), metaNames)
-    survey <- as.data.frame(survey)[,c(metaNames, varNames)]
-    survey$rowID <- NULL
-    return(survey)
+    varNames <- setdiff(names(design), metaNames)
+    design <- as.data.frame(design)[,c(metaNames, varNames)]
+    design$id <- NULL
+    return(design)
 }
 
-addOutsideGood <- function(survey, nAltsPerQ) {
-    survey <- dummyCodeCheck(survey)
+add_no_choice <- function(design, n_alts) {
     # Create outside good rows
-    survey_og <- survey[which(survey$altID == 1),]
-    survey_og[,!names(survey_og)%in%c("respID", "qID", "altID", "obsID")] <- 0
-    survey_og$altID <- nAltsPerQ + 1
-    survey_og$outsideGood <- 1
-    # Add outside good to survey
-    survey$outsideGood <- 0
-    survey <- rbind(survey, survey_og)
-    survey <- survey[order(survey$obsID),]
-    return(survey)
-}
-
-dummyCodeCheck <- function(survey) {
-    types <- getColumnTypes(survey)
-    nonnumeric <- names(types[!types %in% c("integer", "numeric")])
-    if (length(nonnumeric) > 0) {
-        warning(paste0(
-            "The following variables are non-numeric:\n",
-            paste0(nonnumeric, collapse = ", "), "\n\n",
-            "Converting them to dummy-coded variables in order to add ",
-            "outside good"
-        ))
-        survey <- fastDummies::dummy_cols(survey, nonnumeric)
-        survey[,nonnumeric] <- NULL
-    }
-    return(survey)
-}
-
-getColumnTypes <- function(data) {
-  types <- lapply(data, class)
-  test <- function(x) {x[1]}
-  return(unlist(lapply(types, test)))
+    design_og <- design[which(design$altID == 1),]
+    design_og[
+        , !names(design_og) %in% c("respID", "qID", "altID", "obsID")] <- 0
+    design_og$altID <- n_alts + 1
+    design_og$no_choice <- 1
+    # Insert outside good rows in design
+    design$no_choice <- 0
+    design <- rbind(design, design_og)
+    design <- design[order(design$obsID),]
+    return(design)
 }
 
 #' Make a coded matrix of all possible combinations of attribute levels
@@ -268,4 +213,14 @@ cbc_profiles <- function(levels, coding = NULL) {
         return(Profiles(lvls = lvls, coding = coding, c.lvls = clvls))
     }
     return(idefix::Profiles(lvls = lvls, coding = coding))
+}
+
+
+
+
+
+rep_df <- function(df, n) {
+  result <- df[rep(seq_len(nrow(df)), n), ]
+  row.names(result) <- NULL
+  return(result)
 }
