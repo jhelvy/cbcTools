0eb4a061be3217b5ea856d41e6c96e817f48be5a
John Helveston <john.helveston@gmail.com>
2023-02-19 11:08:22 -0500

    added individual level counts to `cbc_balance()`

--- R/inspect.R
+++ R/inspect.R
@@ -44,12 +44,20 @@ cbc_balance <- function(design) {
   # Get pairwise counts matrix for each pair of attributes
   pairs <- data.frame(utils::combn(atts, 2))
   counts_pair <- lapply(pairs, function(x) table(design[[x[1]]], design[[x[2]]]))
-  cat("==============================\n")
+  cat("=====================================\n")
+  cat("Individual attribute level counts\n\n")
+  for (i in 1:length(counts)) {
+      cat(names(counts)[i], ":\n", sep = "")
+      print(counts[[i]])
+      cat("\n")
+  }
+  cat("=====================================\n")
+  cat("Pairwise attribute level counts\n\n")
   for (i in 1:ncol(pairs)) {
       pair_names <- pairs[,i]
       counts1 <- counts[[pair_names[1]]]
       counts2 <- counts[[pair_names[2]]]
-      cat(paste0(pair_names, collapse = " x "), "\n\n")
+      cat(paste0(pair_names, collapse = " x "), ":\n\n", sep = "")
       print(rbind(
           c(NA, counts2),
           cbind(counts1, counts_pair[[i]])
