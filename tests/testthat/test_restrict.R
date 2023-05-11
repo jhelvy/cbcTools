cf5e6a3a402325db8e14f4d5afe74dbf3882fde6
John Helveston <john.helveston@gmail.com>
2023-05-11 16:16:15 -0400

    fixed some issues in tests

--- tests/testthat/test_restrict.R
+++ tests/testthat/test_restrict.R
@@ -1,33 +1,26 @@
 context("Testing cbc_restrict()")
 
-test_that("Restrict profiles based on a single attribute", {
+test_that("Restrict profiles based on a single restriction", {
   profiles <- cbcTools::cbc_profiles(
     price = c(1, 1.5, 2),
     type = c("Fuji", "Gala")
   )
-  restricted_profiles <- cbcTools::cbc_restrict(profiles, type = "Fuji")
-  expected_profiles <- profiles[profiles$type == "Fuji", ]
+  restricted_profiles <- cbcTools::cbc_restrict(
+    profiles,
+    list(type = "Fuji", price = 1.5)
+  )
+  expected_profiles <- as.data.frame(
+     tibble::tribble(
+         ~profileID, ~price,  ~type,
+                 1L,      1, "Fuji",
+                 2L,      2, "Fuji",
+                 3L,      1, "Gala",
+                 4L,    1.5, "Gala",
+                 5L,      2, "Gala"
+    )
+  )
+  expected_profiles$type <- factor(
+    expected_profiles$type, levels = c('Fuji', 'Gala'))
   expect_identical(restricted_profiles, expected_profiles)
 })
 
-test_that("Restrict profiles based on multiple attributes", {
-  profiles <- cbcTools::cbc_profiles(
-    price = c(1, 1.5, 2),
-    type = c("Fuji", "Gala"),
-    freshness = c("Poor", "Excellent")
-  )
-  restricted_profiles <- cbcTools::cbc_restrict(profiles, type = "Fuji", freshness = "Excellent")
-  expected_profiles <- profiles[profiles$type == "Fuji" & profiles$freshness == "Excellent", ]
-  expect_identical(restricted_profiles, expected_profiles)
-})
-
-test_that("Restrict profiles with no matching attributes", {
-  profiles <- cbcTools::cbc_profiles(
-    price = c(1, 1.5, 2),
-    type = c("Fuji", "Gala"),
-    freshness = c("Poor", "Excellent")
-  )
-  restricted_profiles <- cbcTools::cbc_restrict(profiles, type = "Nonexistent")
-  expected_profiles <- profiles[FALSE, ]
-  expect_identical(restricted_profiles, expected_profiles)
-})
